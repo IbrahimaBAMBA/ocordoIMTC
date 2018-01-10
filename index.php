a8b9be06fc8b8bbd79c4d0731a52e278230c0260
Maxime DEFRETIN <maxime.defretin@laposte.net>
2018-01-10 17:32:33 +0100

    Front partie 2

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ if (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] <= $count
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
         <link href="https://fonts.googleapis.com/css?family=Didact+Gothic" rel="stylesheet"> 
         <link rel="stylesheet" href="assets/css/style.css" />
+        <link rel="stylesheet" href="assets/css/style2.css" />
         <title>Projet IMTC</title>
     </head>
     <body>
