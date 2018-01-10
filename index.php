64abdb0a75bd2576c7274243394090e821ff82d6
Chloe Lente <lente.chloe@gmail.com>
2018-01-10 16:27:43 +0100

    mise en forme du CSS

--- index.php
+++ index.php
@@ -20,35 +20,48 @@ if (isset($_GET['page']) && is_numeric($_GET['page']) && $_GET['page'] <= $count
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+        <link href="https://fonts.googleapis.com/css?family=Didact+Gothic" rel="stylesheet"> 
         <link rel="stylesheet" href="assets/css/style.css" />
         <title>Projet IMTC</title>
     </head>
     <body>
-        <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <a class="navLink" href="index.php"></a>
-                <!-- Grâce à getElementsByTagName on récupère les informations du fichier XML -->
-                <?php
-                foreach ($xml->getElementsByTagName('menu') as $link => $index) {
-                    ?>
-                    <!-- On génère l'index afin d'assigner un lien à chaque li de la navbar -->
-                    <li><a href="<?= $link + 1 ?>.html"><?= $index->nodeValue; ?></a></li>
-                    <?php
-                }
-                ?>
-            </ul>
-        </div>
-        <div class="container-fluid">
+        <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <?php
-                    //Récupération du content de la page en fonction de son ID
-                    $targetPage = $xml->getElementsByTagName('content')->item($page);
-                    //nodeValue permet l'envoi du contenu dans la page
-                    echo $targetPage->nodeValue;
-                    ?>
+                    <img class="logo center-block" src="assets/img/logo.png" alt="logo de l'entreprise Ocordo"/>
                 </div>
             </div>
+            
+            <div class="menunavbar collapse navbar-collapse">
+                
+                <ul class="nav navbar-nav">
+                    <a class="navLink" href="index.php"></a>
+                    <!-- Grâce à getElementsByTagName on récupère les informations du fichier XML -->
+                    <?php
+                    foreach ($xml->getElementsByTagName('menu') as $link => $index) {
+                        ?>
+                        <!-- On génère l'index afin d'assigner un lien à chaque li de la navbar -->
+                        <li><a href="<?= $link + 1 ?>.html"><?= $index->nodeValue; ?></a></li>
+                        <?php
+                    }
+                    ?>
+                </ul>
+            </div>
+            <div class="background">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <?php
+                        //Récupération du content de la page en fonction de son ID
+                        $targetPage = $xml->getElementsByTagName('content')->item($page);
+                        //nodeValue permet l'envoi du contenu dans la page
+                        echo $targetPage->nodeValue;
+                        ?>
+                    </div>
+                </div>
+            </div>
+        <footer>
+            <p>blabla</p>
+        </footer>
         </div>
         <script src="assets/js/bootstrap.min.js"></script>
     </body>
